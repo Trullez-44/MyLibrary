650cb3a3194dbcda08441d53da4f05dbdd27062e
Trullez-44 <136148374+Trullez-44@users.noreply.github.com>
2023-09-28 05:22:53 -0500

    refactor: :art: Cleaning and Improving the code
    I delete one useless carpet

--- main.js
+++ main.js
@@ -1,43 +1,42 @@
-import prompts from "prompts";
-const url = "http://127.0.0.1:5444/TESTING-SAPA"
-const data_get=async() => {
+// GET REQUEST FUNCTION
+const url = "http://127.0.0.4:5444/TESTING-SAPA"
+/* const data_get=async() => {
   let data = await(await fetch(url)).json();
-}
-
-const number = await prompts({
-    type: 'number',
-    name: 'value',
-    message: 'Ingrese un numero: '
-  });
-
+  console.log(data[0]['testing-object']);
+} */
+//CALLING
 // data_get();
+//NEW DATA TO POST
 let dict = {
     greetings: 'hi',
-    person:'John',
-    age: 34,
-    //date
+    person:'andys',
+    age: 21,
     date:' today'
 }
-const data_post=async () => {
-  
-    let call = (await fetch("http://127.0.0.1:5444/TESTING-SAPA"))
-    let data = await call.json();
+//POST REQUEST FUNCTION
+const data_post=async (data) => {
+  // console.log(JSON.stringify(data));
     let config ={
       method:"POST",
       header: {"content-type":"application.json"},
-      body: JSON.stringify(dict)
+      body: JSON.stringify(data)
     }
+    console.log(config[2])
+    let sender = await(await fetch(url,config)).json();
+    console.log(sender);
+    console.log('finish')
   }
-data_post();
-const data_put=async (number) => {
-  
-  let call = (await fetch("http://127.0.0.1:5444/TESTING-SAPA"+id))
-  let data = await call.json();
-  let config ={
-    method:"PUT",
-    header:{"content-type":"application.json"},
-    body: JSON.stringify(data)
-  }
-}
+  //CALLING
+data_post(dict);
+/*  */
+// const data_put=async (id) => {  
+//   let call = (await fetch("http://127.0.0.1:5444/TESTING-SAPA"+id))
+//   let data = await call.json();
+//   let config ={
+//     method:"PUT",
+//     header:{"content-type":"application.json"},
+//     body: JSON.stringify(data)
+//   }
+// }
 
-data_put();
+// data_put();
