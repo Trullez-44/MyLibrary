0317dcd1a4cc0f9e126056a22e58eb3cf499f992
Trullez-44 <136148374+Trullez-44@users.noreply.github.com>
2023-10-05 07:25:40 -0500

    feat: :sparkles: POST AND DELETE REQUEST WAS FINISHED

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export default {
+export const uri ={
     protocol : `http://`,
     hostname : `127.0.0.4`,
     port : `5444`
--- modules/books.js
+++ modules/books.js
@@ -1,22 +1,41 @@
-import configURI from '../config.js'
-const uri = `${configURI.protocol + configURI.hostname}:${configURI.port}/`
+import {uri as configURI} from '../config.js'
+const uri = `
+${configURI.protocol +
+configURI.hostname}:
+${configURI.port}/`
 const config ={
     method: undefined,
-    header: {"content-type":"application.json"},
+    headers: {"Content-Type":"application/json"},
     body: undefined
 }
-
-export const GetAll = async (endpoint) => {
+let objeto = {
+    greetings: "Bye",
+    person: "Andrew",
+    age: 21,
+    date:"tomorrow"
+}
+export const getAll = async (endpoint) => {
     config.method = "GET"
     const data  = await (await fetch(`${uri + endpoint}`, config)).json();
     return data
-
 }
-export const Post_Funct = async (endpoint, obj) => {
+export const post_Funct = async (obj) => {
     config.method = "POST"
     config.body = JSON.stringify(obj);
-    const data  = await (await fetch(`${uri + endpoint}`, config)).json();
+    
+    const data  = await (await fetch(`${uri}BOOK`, config)).json();
     return data
 
 }
-getAll('book');
+export const deleteOne = async (endpoint, id) => {
+    if (typeof id !== 'number') return {status: 400, message: 'Invalid Type: ' + id};
+    config.method = "DELETE"
+    const data  = await (await fetch(`${uri + endpoint + '/' + id}`, config)).json();
+    return data
+}
+
+
+
+// post_Funct(objeto);
+console.log(await deleteOne('BOOK', 5));
+
